1431f2715d9b0a31acb2d4e1d27d67aea2becdb8
Peet van de Sande <pvandesande@tmhcc.com>
2021-01-28 21:02:53 +0000

    Updated circumference.py to also calculate area

--- circumference.py
+++ circumference.py
@@ -3,4 +3,8 @@ import math
 
 radius = int(input('What is the radius? '))
 circumference = 2 * math.pi * radius
+area = math.pi * radius ** 2
+# Or go simple
+#area = math.pi * radius * radius
 print(f'The circumference is: {circumference}')
+print(f'The area is: {area}')
